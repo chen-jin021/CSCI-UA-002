d002e14746352f954181d14d3ffdf3545a58475c
Chen Jin <53110673+chen-jin021@users.noreply.github.com>
2020-06-09 13:21:53 +0800

    Create Lecture 2.py

---
+++ Lecture 2.py
@@ -0,0 +1,62 @@
+print('hi')
+print('bye')
+#Python is a linear language
+
+'''
+Below are the delimiters allowed to use:
+""
+''
+""" """
+''' '''
+'''
+
+#Escape Characters
+print("My name is \"craig\"!")
+#Using the backslash "\" to ignore any potential misunderstandings
+print("a backslash: \\")
+#This is how we print a backslash
+print("A\nB\nC\nD")
+#"\n" means new line; it is a line breaker
+print("A\tB\tC")
+#"\t" means tab
+
+#commenting
+#use a hash sign OR ''' '''OR """ """
+
+print("a","b","c") #Python automatically produces the single spaces
+print("c","d","e",sep="***")
+print("there is no longer a line break",end="@")
+print("f","g","h",sep="")
+
+#Variables
+name1="Pikachu"
+name1="Charmander"
+name1="Squirtle"
+print("the name is", name1)
+the_pokemon_name = "Mu"
+thePokemonName = "Generally used in Java and Javascript"
+
+#input function for variables
+name = input("What is your name")
+print("Thanks,",name)
+
+
+#Mad Lib Program
+#Step 1: get four words from the user--noun, verb, adj, adv
+noun = input('noun')
+verb = input('verb')
+adj = input('adj')
+adv = input('adv')
+#Step 2: print story with the values supplied in the correct places
+print("The",adj,noun,"was very hungry",end="")
+print(", so it decided to",adv,verb,"to the nearest restaurant.")
+
+#Math operations
+x=5+3*7/9
+print(x)
+num1= input("num1:")
+num2= input("num2:")
+#Data conversion
+
+print("The answer is", answer)
+#input functions stores strings --- thus users can even enter Pickchu and still work
